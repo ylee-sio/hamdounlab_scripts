c38b33e849859b3e15d47593ca8ceb8688d490c0
unknown <byl006@ucsd.edu>
2022-10-03 05:38:13 -0700

    added feature for including basic montages as a part of standard report

--- image_processing_workflow/transfer_final_images.ps1
+++ image_processing_workflow/transfer_final_images.ps1
@@ -1,65 +1,66 @@
 ﻿cd $HOME
 
-$continue = "y"
+$exp_list = Get-ChildItem -Directory -Name experiments
+$exp_list_length = $exp_list.Length-1
 
-do {
-    $exp_list = Get-ChildItem -Directory -Name experiments
-    $exp_list_length = $exp_list.Length-1
+echo "Available experiments IDs:"
+echo " "
 
-    echo "Available experiments IDs:"
-    echo " "
+foreach($i in 0..$exp_list_length){
+"[$i] " + $exp_list[$i]
+}
+    
+echo " "
+$exp_id_choice = Read-Host -Prompt "Select experiment ID"
+$exp_id = $exp_list[$exp_id_choice]
 
-    foreach($i in 0..$exp_list_length){"[$i] " + $exp_list[$i]}
-    echo " "
-    $exp_id_choice = Read-Host -Prompt "Select experiment ID"
-    $exp_id = $exp_list[$exp_id_choice]
-
-    $exp_id_dir_name = "$HOME/OneDrive - UC San Diego/temp/final_images/$exp_id"
-    if (Test-Path $exp_id_dir_name) {
-	    Write-Host "This directory already exists. Using existing directory."
-        echo ""
-    }
-    else
-    {
+$exp_id_dir_name = "$HOME/OneDrive - UC San Diego/temp/final_images/$exp_id"
+if (Test-Path $exp_id_dir_name) {
+    Write-Host "This directory already exists. Using existing directory."
+    echo ""
+    } else {
 	    New-Item $exp_id_dir_name -ItemType Directory
         echo "Created new directory $exp_id_dir_name"
     }
 
-    $well_id = Read-Host -Prompt "Enter well ID"
-
-    echo "Enter, with USING ONLY ALPHANUMERIC CHARACTERS (no spaces or slashes), genes present in this well"
-    $gene_1 = Read-Host -Prompt "Gene 1"
-    $gene_2 = Read-Host -Prompt "Gene 2"
-    $gene_3 = Read-Host -Prompt "Gene 3"
-    $complete_well_id = $well_id + "_" + $gene_1 + "_" + $gene_2 + "_" + $gene_3
-
-
-    $complete_well_id_dir_name = "$HOME/OneDrive - UC San Diego/temp/final_images/$exp_id/$complete_well_id"
-    if (Test-Path $complete_well_id_dir_name) {
-	    Write-Host "This directory already exists. Using existing directory."
-    }
-    else
-    {
+$well_id = Read-Host -Prompt "Enter well ID"
+echo "Enter, with USING ONLY ALPHANUMERIC CHARACTERS (no spaces or slashes), genes present in this well"
+$gene_1 = Read-Host -Prompt "Gene 1"
+$gene_2 = Read-Host -Prompt "Gene 2"
+$gene_3 = Read-Host -Prompt "Gene 3"
+$complete_well_id = $well_id + "_" + $gene_1 + "_" + $gene_2 + "_" + $gene_3
+$complete_well_id_dir_name = "$HOME/OneDrive - UC San Diego/temp/final_images/$exp_id/$complete_well_id"
+    
+if (Test-Path $complete_well_id_dir_name) {
+    Write-Host "This directory already exists. Using existing directory."
+    } else {
 	    New-Item $complete_well_id_dir_name -ItemType Directory
         echo "Created new directory $well_id_dir_name"
     }
 
-    $site_id = Read-Host -Prompt "Enter site ID"
-    mkdir -p "$HOME/OneDrive - UC San Diego/temp/final_images/$exp_id/$complete_well_id/$site_id"
+$site_id = Read-Host -Prompt "Enter site ID"
 
-    cp -r "$HOME/OneDrive - UC San Diego/temp/temp_imagej_outputs_for_transfer/*"  "$HOME/OneDrive - UC San Diego/temp/final_images/$exp_id/$complete_well_id/$site_id/"
+# make basic_montages
+$image_dir = "$HOME/OneDrive - UC San Diego/temp/temp_imagej_outputs_for_transfer"
+mkdir $image_dir/basic_montages
 
-    echo " "
-    $continue = Read-Host -Prompt "Continue saving other wells/sites? (Enter y or n)"
- 
-    } while ($continue -eq "y")
+montage $image_dir/basic_montages/single_channel/w2_labeled.tif $image_dir/basic_montages/single_channel/w3_labeled.tif $image_dir/basic_montages/single_channel/w4_labeled.tif $image_dir/basic_montages/single_channel/DAPI_labeled.tif -geometry +4+1 $image_dir/basic_montages/montages/single_channel_montage.tif
+montage $image_dir/basic_montages/single_channel/w2_cut_1.tif $image_dir/basic_montages/single_channel/w3_cut_1.tif $image_dir/basic_montages/single_channel/w4_cut_1.tif $image_dir/basic_montages/single_channel/DAPI_cut_1.tif -geometry +4+1 $image_dir/basic_montages/montages/single_channel_montage_cut_1.tif
+montage $image_dir/basic_montages/single_channel_with_dapi/w2_dapi.tif $image_dir/basic_montages/single_channel_with_dapi/w3_dapi.tif $image_dir/basic_montages/single_channel_with_dapi/w4_dapi.tif -geometry +3+1 $image_dir/basic_montages/montages/single_channel_with_dapi_montage.tif
+montage $image_dir/basic_montages/single_channel_with_dapi/w2_DAPI_cut_1.tif $image_dir/basic_montages/single_channel_with_dapi/w3_DAPI_cut_1.tif $image_dir/basic_montages/single_channel_with_dapi/w4_DAPI_cut_1.tif -geometry +3+1 $image_dir/basic_montages/montages/single_channel_with_dapi_montage_DAPI_cut_1.tif
+montage $image_dir/basic_montages/double_channel/w2_w3.tif $image_dir/basic_montages/double_channel/w2_w4.tif $image_dir/basic_montages/double_channel/w3_w4.tif -geometry +3+1 $image_dir/basic_montages/montages/double_channel_montage.tif
+montage $image_dir/basic_montages/double_channel/w2_w3_cut_1.tif $image_dir/basic_montages/double_channel/w2_w4_cut_1.tif $image_dir/basic_montages/double_channel/w3_w4_cut_1.tif -geometry +3+1 $image_dir/basic_montages/montages/double_channel_montage_cut_1.tif
+montage $image_dir/basic_montages/double_channel_with_dapi/w2_w3_DAPI.tif $image_dir/basic_montages/double_channel_with_dapi/w2_w4_DAPI.tif $image_dir/basic_montages/double_channel_with_dapi/w3_w4_DAPI.tif -geometry +3+1 $image_dir/basic_montages/montages/double_channel_with_dapi_montage_DAPI.tif
+montage $image_dir/basic_montages/double_channel_with_dapi/w2_w3_DAPI_cut_1.tif $image_dir/basic_montages/double_channel_with_dapi/w2_w4_DAPI_cut_1.tif $image_dir/basic_montages/double_channel_with_dapi/w3_w4_DAPI_cut_1.tif -geometry +3+1 $image_dir/basic_montages/montages/double_channel_with_dapi_montage_DAPI_cut_1.tif
 
-    $move_to_final_locations = Read-Host -Prompt "Ready to move session files to the Hamdoun Lab official in situ database? (Enter y to transfer files or n to exit.)"
+mkdir -p "$HOME/OneDrive - UC San Diego/temp/final_images/$exp_id/$complete_well_id/$site_id"
+cp -r "$HOME/OneDrive - UC San Diego/temp/temp_imagej_outputs_for_transfer/*"  "$HOME/OneDrive - UC San Diego/temp/final_images/$exp_id/$complete_well_id/$site_id/"
+
+$move_to_final_locations = Read-Host -Prompt "Ready to move session files to the Hamdoun Lab official in situ database? (Enter y to transfer files or n to exit.)"
     
-    if ($move_to_final_locations -eq "y") {
-
-        cp -r  "$HOME/OneDrive - UC San Diego/temp/final_images/$exp_id"  "$HOME/OneDrive - UC San Diego/HL_official_expression_panel_reference_images"
-        mv "$HOME/OneDrive - UC San Diego/temp/final_images/$exp_id"  "D:\HL_official_expression_panel_reference_images"
-        cp -r "$HOME/experiments/$exp_id/$well_id/$site_id" "$HOME/OneDrive - UC San Diego/HL_official_expression_panel_reference_images/$exp_id/$complete_well_id/$site_id/raw_images"
-        $exit = Read-Host -Prompt "Press enter to exit."
+if ($move_to_final_locations -eq "y") {
+    cp -r  "$HOME/OneDrive - UC San Diego/temp/final_images/$exp_id"  "$HOME/OneDrive - UC San Diego/HL_official_expression_panel_reference_images"
+    mv "$HOME/OneDrive - UC San Diego/temp/final_images/$exp_id"  "D:\HL_official_expression_panel_reference_images"
+    cp -r "$HOME/experiments/$exp_id/$well_id/$site_id" "$HOME/OneDrive - UC San Diego/HL_official_expression_panel_reference_images/$exp_id/$complete_well_id/$site_id/raw_images"
+    $exit = Read-Host -Prompt "Press enter to exit."
     }
