21a8685e7f29a1e507bd464bc0a7b0628057a405
unknown <byl006@ucsd.edu>
2022-10-02 23:40:26 -0700

    added more convenient setup scripts

--- image_processing_workflow/setup.ps1
+++ image_processing_workflow/setup.ps1
@@ -1,8 +1,17 @@
 ﻿mkdir $HOME/experiments
-mkdir "C:\Users\Yoon Lee\OneDrive - UC San Diego\temp\final_images"
-mkdir "C:\Users\Yoon Lee\OneDrive - UC San Diego\temp\temp_imagej_outputs_for_transfer\single_channel"
-mkdir "C:\Users\Yoon Lee\OneDrive - UC San Diego\temp\temp_imagej_outputs_for_transfer\single_channel_with_dapi"
-mkdir "C:\Users\Yoon Lee\OneDrive - UC San Diego\temp\temp_imagej_outputs_for_transfer\double_channel"
-mkdir "C:\Users\Yoon Lee\OneDrive - UC San Diego\temp\temp_imagej_outputs_for_transfer\double_channel_with_dapi"
-mkdir "C:\Users\Yoon Lee\OneDrive - UC San Diego\temp\temp_imagej_outputs_for_transfer\triple_channel"
-mkdir "C:\Users\Yoon Lee\OneDrive - UC San Diego\temp\temp_imagej_outputs_for_transfer\triple_channel_with_dapi"
+mkdir "$HOME\OneDrive - UC San Diego\temp\final_images"
+mkdir "$HOME\OneDrive - UC San Diego\temp\temp_imagej_outputs_for_transfer\single_channel"
+mkdir "$HOME\OneDrive - UC San Diego\temp\temp_imagej_outputs_for_transfer\single_channel_with_dapi"
+mkdir "$HOME\OneDrive - UC San Diego\temp\temp_imagej_outputs_for_transfer\double_channel"
+mkdir "$HOME\OneDrive - UC San Diego\temp\temp_imagej_outputs_for_transfer\double_channel_with_dapi"
+mkdir "$HOME\OneDrive - UC San Diego\temp\temp_imagej_outputs_for_transfer\triple_channel"
+mkdir "$HOME\OneDrive - UC San Diego\temp\temp_imagej_outputs_for_transfer\triple_channel_with_dapi"
+mkdir "$HOME\OneDrive - UC San Diego\HL_official_expression_panel_reference_images"
+mkdir $HOME/Applications
+cp -r D:\software\Fiji.app $HOME\Applications
+rm "$HOME\Applications\Fiji.app\macros\StartupMacros.fiji.ijm"
+cp -r D:\hamdounlab_scripts\image_processing_workflow\StartupMacros.fiji.ijm "$HOME\Applications\Fiji.app\macros\"
+Set-ExecutionPolicy RemoteSigned
+New-Alias -Name prep_images -Value ./hamdounlab_scripts/image_processing_workflow/prep_images.ps1
+New-Alias -Name transfer_final_images -Value ./hamdounlab_scripts/image_processing_workflow/transfer_final_images.ps1
+New-Alias -Name cleanup_images -Value ./hamdounlab_scripts/image_processing_workflow/cleanup_images.ps1
