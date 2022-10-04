8874874ed63e7b21fd4da299e3090f05208489fb
unknown <byl006@ucsd.edu>
2022-10-04 13:51:52 -0700

    bug fixes, added feature to correct naming schemes

--- image_processing_workflow/prep_images.ps1
+++ image_processing_workflow/prep_images.ps1
@@ -16,14 +16,13 @@ $user_id = $user_list[$user_id_choice]
 if ($exp_id_list_length -gt 1){
     
 
-    echo "CURRENT STORAGE STATUS: "
-    echo " "
+    echo "CURRENT LOCAL STORAGE STATUS: "
     Get-Volume "C"
     echo " "
 
-    echo "Calculating experiment sizes... This may take longer if experiments have not been transferred to HL_RAID10."
+    echo "Calculating experiment sizes... This may take longer if many experiments have not been transferred to HL_RAID10."
     echo " "
-    echo "Available experiment IDs:"
+    echo "Available experiment IDs to pull from IXM-5155033:"
     echo " "
 
 
@@ -40,15 +39,35 @@ if ($exp_id_list_length -gt 1){
     $exp_id_choice = Read-Host -Prompt "Select number in experiment ID list"
     $exp_id = $exp_id_list[$exp_id_choice]
 
-    if ($exp_id.length != 16){
+    if ($exp_id.length -ne 16){
+        echo ""
         echo "This experiment does not follow standard experiment naming conventions."
         echo "Name needs to be changed to the following format: YYYYMMDD-exp####"
         echo "No file or directory names from the original source will be changed."
         echo "A text file will be generated in this experiment's folder to note this change."
-        $exp_revised_date = Read-Host -Prompt "Please enter the run date of this experiment in the format above"
-        $exp_revised_num = Read-Host -Prompt "Please enter the experiment number in the format above, ignoring the hyphen"
+        echo ""
+        echo "CURRENT EXPERIMENT NAME: $exp_id" 
+        $exp_revised_date = Read-Host -Prompt "`nPlease enter only the RUN DATE of this experiment in this format: YYYYMMDD"
+        
+        while ($exp_revised_date.length -ne 8){
+            echo "**************************************************************************************"
+            echo "`nInvalid entry. Please try again."
+            echo "`nEnter only the run date EXACTLY in this format: exp####"
+            $exp_revised_date = Read-Host -Prompt "`nPlease enter only the RUN DATE of this experiment in this format: YYYYMMDD"
+        }
+
+        $exp_revised_num = Read-Host -Prompt "`nIgnoring the hyphen, Please enter the NEW EXPERIMENT NUMBER in this format: exp####"
+
+        while ($exp_revised_num.length -ne 7){
+            echo "**************************************************************************************"
+            echo "`nInvalid entry. Please try again."
+            echo "`nEnter the experiment number EXACTLY in this format: exp####"
+            $exp_revised_num = Read-Host -Prompt "`nIgnoring the hyphen, Please enter the NEW EXPERIMENT NUMBER in this format: exp####"
+        }
+        
         $revised_exp_id = $exp_revised_date + "-" + $exp_revised_num
-    } 
+        $new_exp_analysis_folder = $revised_exp_id + "_analysis"
+    } else {$new_exp_analysis_folder = $exp_id}
 
 } else {$exp_id = Get-ChildItem -Directory -Name \\Ixm-5155033\f\FileServer\$user_id\*}
 
@@ -87,19 +106,20 @@ if ($run_id_list_length -gt 1){
 
 
 $source_dir = "\\Ixm-5155033\f\FileServer\$user_id\$exp_id\$run_date\$run_id\TimePoint_1"
-$new_exp_analysis_folder = $exp_id + "_analysis"
 mkdir -p experiments/$new_exp_analysis_folder
 $destination = "$HOME/experiments/$new_exp_analysis_folder"
 
 echo "********** STARTING MULTITHREADED COPY FROM SERVER TO LOCAL **********"
 
-robocopy $source_dir $HOME/experiments/$new_exp_analysis_folder /MT:8 /S /E
+robocopy $source_dir $destination /MT:16 /S /E
 rm -r $destination/*.tif
 rm -r $destination/*/*thumb*
 
-if ($exp_id.length != 16){
+if ($exp_id.length -ne 16){
     
     Get-ChildItem -Recurse $destination/*/*.tif | Rename-Item -NewName {$_.Directory.Name + '_' + $revised_exp_id + $_.Name.substring($exp_id.length)}
+    $revision_information = "Original names of experiment files are never changed.`nOnly copies transferred to local experiment_analysis directories are reconfigured.`nThis experiment required file naming revisions.`n`nOriginal base name for files and directories in this experiment: " + $exp_id + "`nRevised base name for files and directories in this experiment: " + $revised_exp_id
+    $revision_information | Out-File -FilePath $destination/revision_information.txt
     
     } else {
 
@@ -107,17 +127,16 @@ if ($exp_id.length != 16){
     
     }
 
-
 $dataset_characterization_file_list = Get-ChildItem -File -Name "$destination\ZStep_1\" -recurse -exclude *thumb*
 $dataset_characterization_file_list_for_wv = Get-ChildItem -File -Name "$destination\ZStep_1\*_s1_*" -recurse -exclude *thumb*
-$well_ids = $dataset_characterization_file_list.Substring(17,3) | Sort-Object | Get-Unique
-$site_ids = $dataset_characterization_file_list.Substring(21,2) | Sort-Object | Get-Unique
-$wv_ids = $dataset_characterization_file_list_for_wv.Substring(24,2)| Sort-Object | Get-Unique
+$well_ids = $dataset_characterization_file_list.Substring(25,3) | Sort-Object | Get-Unique
+$site_ids = $dataset_characterization_file_list.Substring(29,2) | Sort-Object | Get-Unique
+$wv_ids = $dataset_characterization_file_list_for_wv.Substring(32,2)| Sort-Object | Get-Unique
 
 foreach ($i in $well_ids){
     foreach($j in $site_ids){
         foreach ($k in $wv_ids){
-            mkdir -p $HOME/experiments/$new_exp_analysis_folder/$i/$j/$k
+            mkdir -p $destination/$i/$j/$k
         }
      }
    }
@@ -125,16 +144,20 @@ foreach ($i in $well_ids){
 foreach ($i in $well_ids){
     foreach($j in $site_ids){
         foreach ($k in $wv_ids){
-            mv $HOME/experiments/$new_exp_analysis_folder/*/*$i`_$j`_$k* $HOME/experiments/$new_exp_analysis_folder/$i/$j/$k
+            mv $destination/*/*$i`_$j`_$k* $destination/$i/$j/$k
         }
      }
    }
 
-echo "Standard wavelength configuration:"
-echo "w1: transmitted light"
-echo "w2: Cy5 (647nm)"
-echo "w3: TRITC (546nm)"
-echo "w4: FITC (488nm)"
-echo "w5: DAPI (405nm)"
-echo ""
-$correct_wavelength_scheme_check = Read-Host -Prompt "Did this experiment use the standard wavelength configuration?"
+rm -r $destination/Z*
+
+ii $destination
+#echo "Standard wavelength configuration:"
+#echo "w1: transmitted light"
+#echo "w2: Cy5 (647nm)"
+#echo "w3: TRITC (546nm)"
+#echo "w4: FITC (488nm)"
+#echo "w5: DAPI (405nm)"
+#echo ""
+
+#$correct_wavelength_scheme_check = Read-Host -Prompt "Did this experiment use the standard wavelength configuration?"
