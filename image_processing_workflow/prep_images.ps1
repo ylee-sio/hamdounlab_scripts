467bf98f240f53040f1c49836347c6fd6c2599d3
unknown <byl006@ucsd.edu>
2022-10-03 17:59:22 -0700

    simplified image download and preparation script

--- image_processing_workflow/prep_images.ps1
+++ image_processing_workflow/prep_images.ps1
@@ -60,84 +60,34 @@ if ($run_id_list_length -gt 1){
 } else {$run_id = Get-ChildItem -Directory -Name \\Ixm-5155033\f\FileServer\$user_id\$exp_id\$run_date}
 
 
+$source_dir = "\\Ixm-5155033\f\FileServer\$user_id\$exp_id\$run_date\$run_id\TimePoint_1"
+$dataset_characterization_file_list = Get-ChildItem -File -Name "$source_dir\ZStep_1\" -recurse -exclude *thumb*
+$dataset_characterization_file_list_for_wv = Get-ChildItem -File -Name "$source_dir\ZStep_1\*_s1_*" -recurse -exclude *thumb*
+$well_ids = $dataset_characterization_file_list.Substring(17,3) | Sort-Object | Get-Unique
+$site_ids = $dataset_characterization_file_list.Substring(21,2) | Sort-Object | Get-Unique
+$wv_ids = $dataset_characterization_file_list_for_wv.Substring(24,2)| Sort-Object | Get-Unique
+
 $new_exp_analysis_folder = $exp_id + "_analysis"
 mkdir -p experiments/$new_exp_analysis_folder
 
 echo "********** STARTING MULTITHREADED COPY FROM SERVER TO LOCAL **********"
 
-robocopy \\Ixm-5155033\f\FileServer\$user_id\$exp_id\$run_date\$run_id\TimePoint_1 $HOME/experiments/$new_exp_analysis_folder /MT:16 /S /E
-
-echo "********** FINISHED COPYING FILES FROM SERVER TO LOCAL **********"
-echo "********** CLEANING UP LOCAL FILES **********"
-
-rm -r experiments/$new_exp_analysis_folder/*.tif*
-rm -r experiments/$new_exp_analysis_folder/*/*thumb*
+robocopy $source_dir $HOME/experiments/$new_exp_analysis_folder /MT:20 /S /E
 Get-ChildItem -Recurse experiments/$new_exp_analysis_folder/*/*.tif | Rename-Item -NewName {$_.Directory.Name + '_' + $_.Name}
 
-$well_num_list = @('01','02','03','04','05','06','07','08','09','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24')
+foreach ($i in $well_ids){
+    foreach($j in $site_ids){
+        foreach ($k in $wv_ids){
+            mkdir -p $HOME/experiments/$exp_id/$i/$j/$k
+        }
+     }
+   }
 
-mkdir -p experiments/$new_exp_analysis_folder/filtered_sorted
-cd experiments/$new_exp_analysis_folder/filtered_sorted
-$well_num_list | foreach $_{ New-Item -ItemType directory -Name $("A" + $_) }
-$well_num_list | foreach $_{ New-Item -ItemType directory -Name $("B" + $_) }
-$well_num_list | foreach $_{ New-Item -ItemType directory -Name $("C" + $_) }
-$well_num_list | foreach $_{ New-Item -ItemType directory -Name $("D" + $_) }
-$well_num_list | foreach $_{ New-Item -ItemType directory -Name $("E" + $_) }
-$well_num_list | foreach $_{ New-Item -ItemType directory -Name $("F" + $_) }
-$well_num_list | foreach $_{ New-Item -ItemType directory -Name $("G" + $_) }
-$well_num_list | foreach $_{ New-Item -ItemType directory -Name $("H" + $_) }
-$well_num_list | foreach $_{ New-Item -ItemType directory -Name $("I" + $_) }
-$well_num_list | foreach $_{ New-Item -ItemType directory -Name $("J" + $_) }
-$well_num_list | foreach $_{ New-Item -ItemType directory -Name $("K" + $_) }
-$well_num_list | foreach $_{ New-Item -ItemType directory -Name $("L" + $_) }
-$well_num_list | foreach $_{ New-Item -ItemType directory -Name $("M" + $_) }
-$well_num_list | foreach $_{ New-Item -ItemType directory -Name $("N" + $_) }
-$well_num_list | foreach $_{ New-Item -ItemType directory -Name $("O" + $_) }
-$well_num_list | foreach $_{ New-Item -ItemType directory -Name $("P" + $_) }
+#foreach ($i in $well_ids){
+ #   foreach($j in $site_ids){
+  #      foreach ($k in $wv_ids){
+   #         cp -r $HOME/experiments/$new_exp_analysis_folder/*/*$i`_$j`_$k* $HOME/experiments/$exp_id/$i/$j/$k
+    #    }
+     #}
+   #}
 
-$well_folders = ls
-
-foreach ($i in $well_folders){mkdir -p $i/s1}
-foreach ($i in $well_folders){mkdir -p $i/s2}
-foreach ($i in $well_folders){mkdir -p $i/s3}
-foreach ($i in $well_folders){mkdir -p $i/s4}
-foreach ($i in $well_folders){mkdir -p $i/s5}
-foreach ($i in $well_folders){mkdir -p $i/s6}
-foreach ($i in $well_folders){mkdir -p $i/s7}
-foreach ($i in $well_folders){mkdir -p $i/s8}
-foreach ($i in $well_folders){mkdir -p $i/s9}
-foreach ($i in $well_folders){mkdir -p $i/s10}
-foreach ($i in $well_folders){mkdir -p $i/s11}
-foreach ($i in $well_folders){mkdir -p $i/s12}
-
-$site_folders = ls */*/
-foreach ($i in $site_folders) {mkdir -p $i/w1}
-foreach ($i in $site_folders) {mkdir -p $i/w2}
-foreach ($i in $site_folders) {mkdir -p $i/w3}
-foreach ($i in $site_folders) {mkdir -p $i/w4}
-foreach ($i in $site_folders) {mkdir -p $i/w5}
-
-$well_names = Get-ChildItem | 
-       Where-Object {$_.PSIsContainer} | 
-       Foreach-Object {$_.Name}
-
-$site_names = Get-ChildItem A01 | 
-       Where-Object {$_.PSIsContainer} | 
-       Foreach-Object {$_.Name}
-
-$wavelength_names = @('w1', 'w2', 'w3', 'w4', 'w5')
-
-foreach ($k in $wavelength_names) {
-	echo "********** SORTING $k FILES **********"
-	foreach ($i in $well_names) {
-		echo "Sorting well $i"
-		foreach ($j in $site_names) {
-			mv $HOME/experiments/$new_exp_analysis_folder/*/*$i`_$j`_$k* $HOME/experiments/$new_exp_analysis_folder/filtered_sorted/$i/$j/$k/}; 
-	}
-}
-
-cd $HOME
-mv $HOME/experiments/$new_exp_analysis_folder/filtered_sorted/* $HOME/experiments/$new_exp_analysis_folder
-echo "DONE"
-
-ii $HOME/experiments/$new_exp_analysis_folder
